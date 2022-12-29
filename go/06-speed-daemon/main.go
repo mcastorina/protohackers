2ecbbdb9733c707af20189ed0310838b1f50458b
Miccah Castorina <m.castorina93@gmail.com>
2022-12-28 23:08:15 -0600

    Go: calculate speed between observations and return tickets

--- go/06-speed-daemon/main.go
+++ go/06-speed-daemon/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"net"
 	"sync"
 
@@ -14,16 +15,37 @@ type Observation struct {
 	Timestamp uint32
 }
 
+func (o Observation) Speed(other Observation) uint16 {
+	dist := float64(o.Mile) - float64(other.Mile)
+	sec := float64(o.Timestamp) - float64(other.Timestamp)
+
+	if dist == 0 || sec == 0 {
+		return 0
+	}
+
+	// mile/sec -> mile/hour (100x)
+	return uint16(math.Round(360000 * dist / sec))
+}
+
 type ObservationsMap struct {
-	lock                     sync.Mutex
+	lock                     sync.RWMutex
 	plateToRoadToObservation map[string]map[uint16][]Observation
+}
+
+type Ticket struct {
+	plate      string
+	road       uint16
+	mile1      uint16
+	timestamp1 uint32
+	mile2      uint16
+	timestamp2 uint32
+	speed      uint16
 }
 
 func (o *ObservationsMap) addObservation(camera *client.CameraClient, plateInfo *client.PlateInfo) {
 	o.lock.Lock()
 	defer o.lock.Unlock()
-	road := o.plateToRoadToObservation[plateInfo.Plate]
-	if road == nil {
+	if o.plateToRoadToObservation[plateInfo.Plate] == nil {
 		o.plateToRoadToObservation[plateInfo.Plate] = make(map[uint16][]Observation)
 	}
 
@@ -33,6 +55,36 @@ func (o *ObservationsMap) addObservation(camera *client.CameraClient, plateInfo 
 	}
 
 	o.plateToRoadToObservation[plateInfo.Plate][camera.Road] = append(o.plateToRoadToObservation[plateInfo.Plate][camera.Road], observation)
+}
+
+func (o *ObservationsMap) calculateTickets(camera *client.CameraClient, plateInfo *client.PlateInfo) []Ticket {
+	o.lock.RLock()
+	defer o.lock.RUnlock()
+
+	latestObservation := Observation{
+		Mile:      camera.Mile,
+		Timestamp: plateInfo.Timestamp,
+	}
+
+	var tickets []Ticket
+	for _, observation := range o.plateToRoadToObservation[plateInfo.Plate][camera.Road] {
+		if speed := observation.Speed(latestObservation); speed > 100*camera.Limit {
+			ob1, ob2 := observation, latestObservation
+			if ob1.Timestamp > ob2.Timestamp {
+				ob2, ob1 = ob1, ob2
+			}
+			tickets = append(tickets, Ticket{
+				plate:      plateInfo.Plate,
+				road:       camera.Road,
+				mile1:      ob1.Mile,
+				timestamp1: ob1.Timestamp,
+				mile2:      ob2.Mile,
+				timestamp2: ob2.Timestamp,
+				speed:      speed,
+			})
+		}
+	}
+	return tickets
 }
 
 type Ticketmaster struct {
@@ -60,8 +112,10 @@ func main() {
 						// TODO play with plateinfo logic
 						fmt.Println(plateInfo)
 						observationsMap.addObservation(camera, plateInfo)
-						fmt.Printf("%v\n", observationsMap.plateToRoadToObservation)
-
+						if tickets := observationsMap.calculateTickets(camera, plateInfo); len(tickets) > 0 {
+							fmt.Println(tickets)
+						}
+						fmt.Println(observationsMap.plateToRoadToObservation)
 					} else {
 						// Return an error and disconnect.
 						_ = camera.SendError(err)
--- go/06-speed-daemon/test/messages.sh
+++ go/06-speed-daemon/test/messages.sh
@@ -3,12 +3,40 @@
 HOST=localhost
 PORT=1337
 
-function camera {
-    printf '\x80\x00\x42\x00\x64\x00\x3c'
+function camera1 {
+    printf '\x80\x00\x7b\x00\x08\x00\x3c'
 }
 
-function plate {
-    printf '\x20\x04\x55\x4e\x31\x58\x00\x00\x03\xe8'
+function camera2 {
+    printf '\x80\x00\x7b\x00\x09\x00\x3c'
 }
 
-(camera; plate) | nc "$HOST" "$PORT" | xxd
+function plate1 {
+    printf '\x20\x04\x55\x4e\x31\x58\x00\x00\x00\x00'
+}
+
+function plate2 {
+    printf '\x20\x04\x55\x4e\x31\x58\x00\x00\x00\x2d'
+}
+
+# Hexadecimal:
+# <-- 80 00 7b 00 08 00 3c
+# <-- 20 04 55 4e 31 58 00 00 00 00
+
+# Decoded:
+# <-- IAmCamera{road: 123, mile: 8, limit: 60}
+# <-- Plate{plate: "UN1X", timestamp: 0}
+
+(camera1; plate1) | nc "$HOST" "$PORT" &
+
+# Client 2: camera at mile 9
+
+# Hexadecimal:
+# <-- 80 00 7b 00 09 00 3c
+# <-- 20 04 55 4e 31 58 00 00 00 2d
+
+# Decoded:
+# <-- IAmCamera{road: 123, mile: 9, limit: 60}
+# <-- Plate{plate: "UN1X", timestamp: 45}
+
+(camera2; plate2) | nc "$HOST" "$PORT" &
