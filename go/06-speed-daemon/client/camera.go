8c2828aa3bc739e50b4b2dc2a62709ac2955ce34
jobin212 <jobin212@gmail.com>
2022-12-28 23:30:07 -0500

    Start recording observations

--- go/06-speed-daemon/client/camera.go
+++ go/06-speed-daemon/client/camera.go
@@ -14,6 +14,17 @@ type PlateInfo struct {
 	Timestamp uint32
 }
 
+// plate -> road -> [(mile, timestamp), ...]
+
+// TICKETMASTER
+// plate -> day -> ticket
+// day -> plate -> ticket
+
+// plateABC : {
+// 	"road_123" : [(mile_123, ts_456), (mile_324, ts_457), (mile_789, ts_458)],
+// 	"road_5345": [324, 545, 39],
+// }
+
 func (c *CameraClient) ReadPlate() (*PlateInfo, error) {
 	kind, err := c.ReadU8()
 	if err != nil {
--- go/06-speed-daemon/main.go
+++ go/06-speed-daemon/main.go
@@ -4,14 +4,50 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"sync"
 
 	"06-speed-daemon/client"
 )
+
+type Observation struct {
+	Mile      uint16
+	Timestamp uint32
+}
+
+type ObservationsMap struct {
+	lock                     sync.Mutex
+	plateToRoadToObservation map[string]map[uint16][]Observation
+}
+
+func (o *ObservationsMap) addObservation(camera *client.CameraClient, plateInfo *client.PlateInfo) {
+	o.lock.Lock()
+	defer o.lock.Unlock()
+	road := o.plateToRoadToObservation[plateInfo.Plate]
+	if road == nil {
+		o.plateToRoadToObservation[plateInfo.Plate] = make(map[uint16][]Observation)
+	}
+
+	observation := Observation{
+		Mile:      camera.Mile,
+		Timestamp: plateInfo.Timestamp,
+	}
+
+	o.plateToRoadToObservation[plateInfo.Plate][camera.Road] = append(o.plateToRoadToObservation[plateInfo.Plate][camera.Road], observation)
+}
+
+type Ticketmaster struct {
+	lock                sync.Mutex
+	dayToPlatesObserved map[string]map[string]bool
+}
 
 func main() {
 	server, err := NewServer()
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	observationsMap := ObservationsMap{
+		plateToRoadToObservation: make(map[string]map[uint16][]Observation),
 	}
 
 	for conn := range server.Connections() {
@@ -23,6 +59,9 @@ func main() {
 					if plateInfo, err := camera.ReadPlate(); err == nil {
 						// TODO play with plateinfo logic
 						fmt.Println(plateInfo)
+						observationsMap.addObservation(camera, plateInfo)
+						fmt.Printf("%v\n", observationsMap.plateToRoadToObservation)
+
 					} else {
 						// Return an error and disconnect.
 						_ = camera.SendError(err)
