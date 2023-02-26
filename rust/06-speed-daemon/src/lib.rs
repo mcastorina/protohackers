d9e53bb8d4dab62f0830d2fe7f83eaf01c1e422f
Miccah Castorina <m.castorina93@gmail.com>
2023-02-25 21:10:27 -0600

    Implement read_id and read_message relying on TCP read timeouts
    In an attempt to not use an async framework, I'm going to rely on read
    timeouts to poll whether a message is available or not. It has the
    potential to fail and lose data, but it should pass the tests.

--- rust/06-speed-daemon/src/lib.rs
+++ rust/06-speed-daemon/src/lib.rs
@@ -20,18 +20,43 @@ struct Client<R: Read, W: Write, Kind = Common> {
 }
 
 impl<R: Read, W: Write> Client<R, W> {
-    fn next_message(&mut self) -> Result<msg::IncomingMessage, Box<dyn Error>> {
+    fn next_message(&mut self) -> Result<Option<msg::IncomingMessage>, Box<dyn Error>> {
         use msg::IncomingMessage::*;
 
-        let mut id = 0;
-        self.rbuf.read_exact(slice::from_mut(&mut id))?;
-
-        Ok(match id {
-            msg::WantHeartbeat::ID => WantHeartbeat(codec::from_reader(&mut self.rbuf)?),
-            msg::IAmCamera::ID => IAmCamera(codec::from_reader(&mut self.rbuf)?),
-            msg::IAmDispatcher::ID => IAmDispatcher(codec::from_reader(&mut self.rbuf)?),
-            _ => return Err("unrecognized message".into()),
+        let id = self.read_id()?;
+        // If the client is too slow to provide all the bytes of a message, an error will be
+        // returned and the order of messages possibly misaligned. That shouldn't matter though,
+        // because an error here usually means the client should disconnect.
+        id.map(|id| match id {
+            msg::WantHeartbeat::ID => Ok(WantHeartbeat(self.read_message()?)),
+            msg::IAmCamera::ID => Ok(IAmCamera(self.read_message()?)),
+            msg::IAmDispatcher::ID => Ok(IAmDispatcher(self.read_message()?)),
+            _ => Err("unrecognized message".into()),
         })
+        .transpose()
+    }
+
+    // TODO: run_once can return another implementation of Kind
+    fn run_once(&mut self) -> Result<(), Box<dyn Error>> {
+        self.send_heartbeat()?;
+        let msg = self.next_message()?;
+        if msg.is_none() {
+            // Nothing to do.
+            return Ok(());
+        }
+        match msg.unwrap() {
+            msg::IncomingMessage::WantHeartbeat(want_heartbeat) => {
+                self.want_heartbeat(want_heartbeat)?
+            }
+            msg::IncomingMessage::IAmCamera(_) => {
+                todo!()
+            }
+            msg::IncomingMessage::IAmDispatcher(_) => {
+                todo!()
+            }
+            _ => unreachable!(),
+        }
+        Ok(())
     }
 
     fn into_camera(self) -> Client<R, W, Camera> {
@@ -54,31 +79,35 @@ impl<R: Read, W: Write> Client<R, W> {
 }
 
 impl<R: Read, W: Write> Client<R, W, Camera> {
-    fn next_message(&mut self) -> Result<msg::IncomingMessage, Box<dyn Error>> {
+    fn next_message(&mut self) -> Result<Option<msg::IncomingMessage>, Box<dyn Error>> {
         use msg::IncomingMessage::*;
 
-        let mut id = 0;
-        self.rbuf.read_exact(slice::from_mut(&mut id))?;
-
-        Ok(match id {
-            msg::Plate::ID => Plate(codec::from_reader(&mut self.rbuf)?),
-            msg::WantHeartbeat::ID => WantHeartbeat(codec::from_reader(&mut self.rbuf)?),
-            _ => return Err("unrecognized message".into()),
+        let id = self.read_id()?;
+        // If the client is too slow to provide all the bytes of a message, an error will be
+        // returned and the order of messages possibly misaligned. That shouldn't matter though,
+        // because an error here usually means the client should disconnect.
+        id.map(|id| match id {
+            msg::Plate::ID => Ok(Plate(self.read_message()?)),
+            msg::WantHeartbeat::ID => Ok(WantHeartbeat(self.read_message()?)),
+            _ => Err("unrecognized message".into()),
         })
+        .transpose()
     }
 }
 
 impl<R: Read, W: Write> Client<R, W, Dispatcher> {
-    fn next_message(&mut self) -> Result<msg::IncomingMessage, Box<dyn Error>> {
+    fn next_message(&mut self) -> Result<Option<msg::IncomingMessage>, Box<dyn Error>> {
         use msg::IncomingMessage::*;
 
-        let mut id = 0;
-        self.rbuf.read_exact(slice::from_mut(&mut id))?;
-
-        Ok(match id {
-            msg::WantHeartbeat::ID => WantHeartbeat(codec::from_reader(&mut self.rbuf)?),
-            _ => return Err("unrecognized message".into()),
+        let id = self.read_id()?;
+        // If the client is too slow to provide all the bytes of a message, an error will be
+        // returned and the order of messages possibly misaligned. That shouldn't matter though,
+        // because an error here usually means the client should disconnect.
+        id.map(|id| match id {
+            msg::WantHeartbeat::ID => Ok(WantHeartbeat(self.read_message()?)),
+            _ => Err("unrecognized message".into()),
         })
+        .transpose()
     }
 }
 
@@ -103,10 +132,11 @@ impl<R: Read, W: Write, Kind> Client<R, W, Kind> {
         Ok(())
     }
 
-    // TODO: run_once can return another implementation of Kind
-    fn run_once(&mut self) -> Result<(), Box<dyn Error>> {
-        if let Some((period, last)) = self.heartbeat {
-            if last.elapsed() > period {
+    fn send_heartbeat(&mut self) -> Result<(), Box<dyn Error>> {
+        match self.heartbeat {
+            None => (),
+            Some((period, last)) if last.elapsed() < period => (),
+            Some((period, last)) => {
                 msg::Heartbeat.to_writer(&mut self.wbuf)?;
                 let mut next = last;
                 while next + period < time::Instant::now() {
@@ -116,6 +146,30 @@ impl<R: Read, W: Write, Kind> Client<R, W, Kind> {
             }
         }
         Ok(())
+    }
+
+    fn read_id(&mut self) -> Result<Option<u8>, Box<dyn Error>> {
+        let mut id = 0;
+        match self.rbuf.read_exact(slice::from_mut(&mut id)) {
+            Ok(_) => Ok(Some(id)),
+            Err(err) if err.kind() == io::ErrorKind::WouldBlock => Ok(None),
+            Err(err) => Err(err.into()),
+        }
+    }
+
+    fn read_message<'de, T: serde::Deserialize<'de>>(&mut self) -> Result<T, Box<dyn Error>> {
+        // By this point we should already have received a message ID, so reading the message
+        // shouldn't fail. A WouldBlock error means the client was too slow sending the data and we
+        // partially read the message, losing bytes and getting in an inconsistent state. Since
+        // this is soley for protohackers, this should be fine. Otherwise, a proper async library
+        // should be used.
+        let result = codec::from_reader(&mut self.rbuf);
+        if let Err(codec::Error::IOError(ref err)) = result {
+            if err.kind() == io::ErrorKind::WouldBlock {
+                panic!("timed out reading message and lost data!");
+            }
+        }
+        Ok(result?)
     }
 }
 
@@ -141,15 +195,15 @@ fn common_next_message() {
 
     let mut client = Client::new(&input[..], io::sink());
     assert_eq!(
-        client.next_message().unwrap(),
+        client.next_message().unwrap().unwrap(),
         msg::IncomingMessage::WantHeartbeat(heartbeat)
     );
     assert_eq!(
-        client.next_message().unwrap(),
+        client.next_message().unwrap().unwrap(),
         msg::IncomingMessage::IAmCamera(camera)
     );
     assert_eq!(
-        client.next_message().unwrap(),
+        client.next_message().unwrap().unwrap(),
         msg::IncomingMessage::IAmDispatcher(dispatcher)
     );
     assert!(client.next_message().is_err());
@@ -180,15 +234,15 @@ fn into_camera() {
 
     let mut client = Client::new(&input[..], io::sink()).into_camera();
     assert_eq!(
-        client.next_message().unwrap(),
+        client.next_message().unwrap().unwrap(),
         msg::IncomingMessage::Plate(plates[0].clone())
     );
     assert_eq!(
-        client.next_message().unwrap(),
+        client.next_message().unwrap().unwrap(),
         msg::IncomingMessage::Plate(plates[1].clone())
     );
     assert_eq!(
-        client.next_message().unwrap(),
+        client.next_message().unwrap().unwrap(),
         msg::IncomingMessage::Plate(plates[2].clone())
     );
 }
@@ -196,7 +250,7 @@ fn into_camera() {
 #[test]
 fn test_heartbeat() {
     let mut output = Vec::new();
-    let mut client = Client::new(&[][..], &mut output);
+    let mut client = Client::new(io::empty(), &mut output);
     client
         .want_heartbeat(msg::WantHeartbeat {
             interval: msg::Decisecond(1),
